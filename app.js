0526ea7167918d80210c18f9a2d203776ab56426
Jamilo-school <geofreyonyango167@gmail.com>
2023-08-31 09:18:26 +0300

    K

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ loginForm.addEventListener("submit", (event) => {
     const greeting = `${currentDay}, ${currentTime}. Welcome ${name}! Teacher of ${subjects}. ${location ? location : ''} Jamilo School`;
 
     alert(greeting);
-    window.location.href = "https://jamilo-school.github.io/landing-page./";
+    window.location.href = "https://jamilo-school.github.io/Main-page/";
   } else {
     alert("🎯 Access denied! You are trying to use unauthorized credentials. Please check and try again ");
   }
