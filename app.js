a0da9adfa755bdf33d6e12eb98a6c7871817c21f
Jamilo-school <geofreyonyango167@gmail.com>
2023-10-15 13:10:45 +0300

    ohmn

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ loginForm.addEventListener("submit", (event) => {
     // Redirect to www.google.com after 3 seconds
     setTimeout(() => {
       window.location.href = "https://jamilo-school.github.io/landing-page/";
-    }, 6000); // 3000 milliseconds = 3 seconds
+    }, 8000); // 3000 milliseconds = 3 seconds
   } else {
     // Play the error sound
     errorSound.play();
@@ -116,7 +116,7 @@ loginForm.addEventListener("submit", (event) => {
   // Automatically hide the popup after 3 seconds
   setTimeout(() => {
     hideMessage();
-  }, 5000); // 3000 milliseconds = 3 seconds
+  }, 7000); // 3000 milliseconds = 3 seconds
 });
 
 popupClose.addEventListener("click", () => {
